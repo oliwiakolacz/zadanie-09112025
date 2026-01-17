bf63fb44a3c87801ac11548857db4b7812e368ab
Oliwia Kolacz <oliwia.k.kolacz@gmail.com>
2026-01-17 22:47:49 +0100

    Dodaje express.static

--- back/server.js
+++ back/server.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../front')));
 
 // Ścieżka do pliku z danymi
 const filePath = path.join(__dirname, 'data.json');
